6d03830dead98154be57f75a0a9649af28e29a24
ll799268 <1010023095@qq.com>
2021-03-12 14:36:45 +0800

    add 两数之和

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/** 两数之和
+/** 
+ * 两数之和
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
@@ -16,4 +17,40 @@ var twoSum = function (nums, target) {
   return arr
 };
 
-// console.log(twoSum([3, 3], 6));
+/**
+ * 两数之和
+ * @param {*} l1 
+ * @param {*} l2 
+ * @returns 
+ */
+var addTwoNumbers = function (l1, l2) {
+  if (l1.length === l2.length) {
+    var arr = []
+    for (var i = 0; i < l1.length; i++) {
+      var item1 = l1[i],
+        item2 = l2[i],
+        sum = item1 + item2
+
+      arr[i] === 1 ? '' : arr[i] = 0
+
+      if (sum >= 10) {
+        i === l1.length ? '' :  arr[i + 1] = 1
+      } else {
+        arr[i] += sum
+      }
+      
+    }
+    
+    return arr.reverse()
+  } else {
+    var l1Number = parseInt(l1.join(',').replace(/\,/g, '')),
+      l2Number = parseInt(l2.join(',').replace(/\,/g, '')),
+      result = (l1Number + l2Number).toString()
+                                    .split('')
+                                    .map(item => +item)
+                                    .reverse()
+
+    return result
+  }
+
+};
