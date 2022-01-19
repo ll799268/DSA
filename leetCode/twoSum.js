04873c0ff199ceb2295064a7976bad4c41e86b99
ll799268 <1010023095@qq.com>
2022-01-19 15:12:14 +0800

    两数之和

---
+++ leetCode/twoSum.js
@@ -0,0 +1,26 @@
+/** 两数之和
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+    var firstNum = nums[i]
+    for (var j = 0; j < nums.length; j++) {
+      var lastNum = nums[j]
+      if (i !== j && firstNum + lastNum === target) {
+        return [i, j]
+      }
+    }
+  }
+};
+
+var twoSum = function (nums, target) {
+  const map = new Map;
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) return [i, map.get(nums[i])]
+    map.set(target - nums[i], i)
+  }
+};
+
+console.log(twoSum([3, 3], 6));
