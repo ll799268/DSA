25fcc4ac296af0e53be2c2568d6d07b102b48c43
ll799268 <1010023095@qq.com>
2022-01-20 17:48:01 +0800

    合并两个有效链表

---
+++ leetCode/mergeTwoLists.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *   this.val = (val===undefined ? 0 : val)
+ *   this.next = (next===undefined ? null : next)
+ * }
+ */
+/** 合并两个有效链表
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  let cur = new ListNode() // 定义链表
+  let head = cur
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      cur.next = list1
+      list1 = list1.next
+    } else {
+      cur.next = list2
+      list2 = list2.next
+    }
+    cur = cur.next
+  }
+  if (!list1) cur.next = list2
+  if (!list2) cur.next = list1
+
+  return head.next
+}
+
+console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
