50818077c292e9ec80ae8007160789785eb1f321
ll799268 <1010023095@qq.com>
2022-01-21 16:20:12 +0800

    移除元素

---
+++ leetCode/removeElement.js
@@ -0,0 +1,26 @@
+/** 移除元素
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1)
+      i--
+    }
+  }
+  return nums.length
+}
+
+var removeElement = function (nums, val) {
+  let k = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k++] = nums[i]
+    }
+  }
+  return k
+}
+
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
