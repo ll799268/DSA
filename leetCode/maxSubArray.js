bdf3c297a84b38a791d39a181e117b9be897dff9
ll799268 <1010023095@qq.com>
2022-02-10 13:55:24 +0800

    最大子数组和

---
+++ leetCode/maxSubArray.js
@@ -0,0 +1,33 @@
+/** 最大子数组和
+ * 分治法
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = nums => {
+  // 数组长度，dp初始化
+  const [len, dp] = [nums.length, [nums[0]]]
+  // 最大值初始化为dp[0]
+  let max = dp[0]
+  for (let i = 1; i < len; i++) {
+    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i])
+    // 更新最大值
+    max = Math.max(max, dp[i])
+  }
+  return max
+}
+
+// 贪心算法
+var maxSubArray = nums => {
+  let currySum = nums[0],
+    maxSum = nums[0]
+
+  for (let i = 1; i < nums.length; i++) {
+    const curry = nums[i]
+    currySum = Math.max(curry, currySum + curry)
+    maxSum = Math.max(maxSum, currySum)
+  }
+
+  return maxSum
+}
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
