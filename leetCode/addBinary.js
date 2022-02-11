c5c7d64cf539c0fb99e09eaf24bf2fceb8a605eb
ll799268 <1010023095@qq.com>
2022-02-11 17:54:50 +0800

    二进制求和

---
+++ leetCode/addBinary.js
@@ -0,0 +1,23 @@
+/** 二进制求和
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+  let str = ''
+  let i = a.length - 1, j = b.length - 1
+  let dight = 0, cary = 0, sum = 0
+  while (i >= 0 || j >= 0) {
+    sum = ~~a[i] + ~~b[j] + cary
+    dight = sum % 2
+    cary = (sum - dight) / 2
+    str = dight + str
+    i--
+    j--
+  }
+  return cary ? cary + str : str
+}
+
+console.log(addBinary('1010', '1011'))
+
+// 10101
