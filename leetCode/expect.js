0b26b5a70a56ddeac0d7ed0305ce713a8820effc
ll799268 <1010023095@qq.com>
2023-12-11 16:44:57 +0800

    feat: 相等还是不相等

---
+++ leetCode/expect.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+  this.toBe = propVal => {
+    if (val === propVal) return true
+    throw new Error('Not Equal')
+  }
+  this.notToBe = propVal => {
+    if (val !== propVal) return true
+    throw new Error('Equal')
+  }
+
+  return this
+};
+
+/**
+* expect(5).toBe(5); // true
+* expect(5).notToBe(5); // throws "Equal"
+*/
