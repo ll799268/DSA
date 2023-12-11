b46c8abab26366d1c6c396066c7077cced0eb71d
ll799268 <1010023095@qq.com>
2023-12-11 17:01:40 +0800

    feat: 扁平化数组

---
+++ leetCode/flat.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+  if (n === 0) return arr
+  const result = []
+
+  for (let i = 0; i < arr.length; i++) {
+    result.push(...(Array.isArray(arr[i]) ? flat(arr[i], n - 1) : [arr[i]]));
+  }
+  return result
+};
