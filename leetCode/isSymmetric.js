e12144286bb318a659b8fd2858b2a029baa0f571
ll799268 <1010023095@qq.com>
2023-12-11 18:18:03 +0800

    feat: 对称二叉树

---
+++ leetCode/isSymmetric.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  const compareNode = (left, right) => {
+    if (left === null && right === null) return true
+    if (left === null && right !== null || (left !== null && right === null)) return false
+    if (left.val !== right.val) return false
+    return compareNode(left.left, right.right) && compareNode(left.right, right.left)
+  }
+
+  return compareNode(root.left, root.right)
+};
