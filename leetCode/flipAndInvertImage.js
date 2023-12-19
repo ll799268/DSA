0118b94d6a72172713e340922bf11824d051beb1
ll799268 <1010023095@qq.com>
2023-12-19 23:45:39 +0800

    feat: 翻转图像

--- leetCode/flipAndInvertImage.js
+++ leetCode/flipAndInvertImage.js
@@ -13,4 +13,8 @@ var flipAndInvertImage = function (image) {
   })
 };
 
+var flipAndInvertImage = function (image) {
+  return image.map(item => item.reverse().map(c => c ^ 1))
+};
+
 console.log(flipAndInvertImage([[1, 1, 0], [1, 0, 1], [0, 0, 0]]));
