2ba1d4201a04796bf3e58ceb6d516b5eba56f9a9
ll799268 <1010023095@qq.com>
2023-12-25 21:28:21 +0800

    update: 2的幂

--- leetCode/isPowerOfTwo.js
+++ leetCode/isPowerOfTwo.js
@@ -3,8 +3,19 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfTwo = function(n) {
+var isPowerOfTwo = function (n) {
   if (n === 1) return true
   if (n <= 0) return false
   return isPowerOfTwo(n / 2)
+};
+
+var isPowerOfTwo = function (n) {
+  if (n === 1) return true
+
+  while (n >= 1) {
+    if (n / 2 === 1) return true
+    n /= 2
+  }
+
+  return false
 };
