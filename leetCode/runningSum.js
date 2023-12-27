2f51fe3601f1d05b479ce4ccc24a16f0fab37cc0
ll799268 <1010023095@qq.com>
2023-12-27 21:51:14 +0800

    feat: 一维数组的动态和

---
+++ leetCode/runningSum.js
@@ -0,0 +1,11 @@
+/**
+ * 一维数组的动态和
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = nums[i - 1] + nums[i]
+  }
+  return nums
+};
