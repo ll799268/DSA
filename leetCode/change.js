1e02ee76a09297968e5f8da9a0dd0f824952254f
ll799268 <1010023095@qq.com>
2023-12-27 19:16:09 +0800

    feat: 零钱兑换II

---
+++ leetCode/change.js
@@ -0,0 +1,20 @@
+/**
+ * 零钱兑换II
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+  const dp = new Array(amount + 1).fill(0)
+  dp[0] = 1
+
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] += dp[i - coin]
+    }
+  }
+
+  return dp[amount]
+};
+
+console.log(change(5, [1, 2, 5]));
