58cc64546ee3c5d5ffcfe1e60ce793ef8cfe5d5c
ll799268 <1010023095@qq.com>
2023-12-27 19:16:16 +0800

    feat: 零钱兑换

---
+++ leetCode/coinChange.js
@@ -0,0 +1,21 @@
+/**
+ * 零钱兑换
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity)
+  dp[0] = 0
+
+  for (let i = 0; i <= amount; i++) {
+    for (let coin of coins) {
+      if (i - coin < 0) continue
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1)
+    }
+  }
+
+  return dp[amount] === Infinity ? -1 : dp[amount];
+};
+
+console.log(coinChange([1, 2, 5], 11));
