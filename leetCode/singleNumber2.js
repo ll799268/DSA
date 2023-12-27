2ecc3f1b964e261420281498d066761e09848fc5
ll799268 <1010023095@qq.com>
2023-12-27 21:38:59 +0800

    feat: 只出现一次的数字

---
+++ leetCode/singleNumber2.js
@@ -0,0 +1,16 @@
+/**
+ * 只出现一次的数字
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  const map = new Map()
+  for (const k of nums) {
+    const count = map.get(k)
+    count ? map.set(k, count + 1) : map.set(k, 1)
+  }
+
+  for (const [k, v] of map) {
+    if (v === 1) return k
+  }
+};
