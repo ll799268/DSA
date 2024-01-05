575a954986f91824c468ee89d172feb600dd90e5
ll799268 <1010023095@qq.com>
2024-01-05 19:11:41 +0800

    feat: 判断能否形成等差数列

---
+++ leetCode/canMakeArithmeticProgression.js
@@ -0,0 +1,17 @@
+/**
+ * 判断能否形成等差数列
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function (arr) {
+  arr.sort((a, b) => a - b)
+  const diff = arr[1] - arr[0]
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== diff) {
+      return false
+    }
+  }
+
+  return true
+};
