51a38d5f50aba4e967b2e8f59afc763abbe359c3
ll799268 <1010023095@qq.com>
2024-01-06 22:42:57 +0800

    feat: 有多少小于当前数字的数字

---
+++ leetCode/smallerNumbersThanCurrent.ts
@@ -0,0 +1,33 @@
+/**
+ * 有多少小于当前数字的数字
+ * @param nums 
+ * @returns 
+ */
+function smallerNumbersThanCurrent(nums: number[]): number[] {
+  let result: number[] = []
+
+  for (let i = 0; i < nums.length; i++) {
+    const cur: number = nums[i]
+    const count: number = nums.filter(num => num < cur).length
+    result.push(count)
+  }
+
+  return result
+};
+
+function smallerNumbersThanCurrent1(nums: number[]): number[] {
+  let result: number[] = []
+
+  for (let i = 0; i < nums.length; i++) {
+    const cur: number = nums[i]
+    let count: number = 0
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[j] < cur) {
+        count++
+      }
+    }
+    result.push(count)
+  }
+
+  return result
+};
