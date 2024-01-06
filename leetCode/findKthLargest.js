0a511f8d76de06d7085bdc6b33f3855975a28055
ll799268 <1010023095@qq.com>
2024-01-06 15:05:05 +0800

    feat: 数组中的第K个最大元素

---
+++ leetCode/findKthLargest.js
@@ -0,0 +1,9 @@
+/**
+ * 数组中的第K个最大元素
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+  return nums.sort((a, b) => b - a)[k - 1]
+};
