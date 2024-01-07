28889a317bfac0104f28e1e73f88ece7af40bd4b
ll799268 <1010023095@qq.com>
2024-01-07 15:36:40 +0800

    feat: 将找到的值乘以2

---
+++ leetCode/findFinalValue.ts
@@ -0,0 +1,19 @@
+/**
+ * 将找到的值乘以 2
+ * @param nums 
+ * @param original 
+ * @returns 
+ */
+function findFinalValue(nums: number[], original: number): number {
+  return nums.includes(original) ? findFinalValue(nums, original * 2) : original
+};
+
+function findFinalValue1(nums: number[], original: number): number {
+  nums.sort((a, b) => a - b)
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === original) original *= 2
+  }
+
+  return original
+};
