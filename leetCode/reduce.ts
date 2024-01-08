7a62a00b5308003bbbd2e69a24ceb5694a2f995c
ll799268 <1010023095@qq.com>
2024-01-08 22:16:56 +0800

    feat: 数组规约运算

--- leetCode/reduce.ts
+++ leetCode/reduce.ts
@@ -1,9 +1,5 @@
 /**
  * 数组规约运算
- * @param {number[]} nums
- * @param {Function} fn
- * @param {number} init
- * @return {number}
  */
 function reduce(nums: number[], fn: (accum: number, curr: number) => number, init: number): number {
   let result: number = init
