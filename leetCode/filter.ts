0b644f950e8134e722731c230e585e0ee179c92a
ll799268 <1010023095@qq.com>
2024-01-08 21:50:25 +0800

    feat: 过滤数组中的元素

--- leetCode/filter.ts
+++ leetCode/filter.ts
@@ -1,11 +1,14 @@
+
+type Fn = (n: number, i: number) => any
+
 /**
  * 过滤数组中的元素
  * @param {number[]} arr
  * @param {Function} fn
  * @return {number[]}
  */
-var filter = function (arr, fn) {
-  let result = []
+function filter(arr: number[], fn: Fn): number[] {
+  let result: number[] = []
   for (let i = 0; i < arr.length; i++) {
     fn(arr[i], i) && result.push(arr[i])
   }
