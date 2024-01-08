76257e97816b94753771661efc710cbc9dff28d8
ll799268 <1010023095@qq.com>
2024-01-08 21:21:09 +0800

    feat: 数组原型对象的最后一个元素

--- leetCode/arrPrototypeLast.ts
+++ leetCode/arrPrototypeLast.ts
@@ -1,12 +1,19 @@
 /**
  * 数组原型对象的最后一个元素
- * @return {null|boolean|number|string|Array|Object}
  */
+declare global {
+  interface Array<T> {
+    last(): T | -1;
+  }
+}
+
 Array.prototype.last = function () {
-  return this.length === 0 ? -1 : this[this.length - 1]
+  return this.length ? this[this.length - 1] : -1
 };
 
 /**
 * const arr = [1, 2, 3];
 * arr.last(); // 3
 */
+
+export { };
