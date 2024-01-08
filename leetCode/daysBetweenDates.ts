7f046b49c2fe5570cfe75484a7014e29d797c6ef
ll799268 <1010023095@qq.com>
2024-01-08 22:15:47 +0800

    feat: 日期间隔几天

--- leetCode/daysBetweenDates.ts
+++ leetCode/daysBetweenDates.ts
@@ -1,8 +1,5 @@
 /**
  * 日期间隔几天
- * @param date1 
- * @param date2 
- * @returns 
  */
 function daysBetweenDates(date1: string, date2: string): number {
   const diff: number = new Date(date1).getTime() - new Date(date2).getTime()
