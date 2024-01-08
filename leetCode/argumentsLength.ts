b0d79a9bdf8f8532bf0fffa8b2fbb0fbef91a645
ll799268 <1010023095@qq.com>
2024-01-08 22:23:07 +0800

    feat: 返回传递的参数的长度

---
+++ leetCode/argumentsLength.ts
@@ -0,0 +1,13 @@
+/**
+ * 返回传递的参数的长度
+ */
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+  return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
