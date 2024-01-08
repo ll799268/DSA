1f1ca8975a0f3fedab1c964f24ed93ad5c0efa4b
ll799268 <1010023095@qq.com>
2024-01-08 17:52:52 +0800

    feat: 回旋镖的数量

---
+++ leetCode/numberOfBoomerangs.ts
@@ -0,0 +1,19 @@
+/**
+ * 回旋镖的数量
+ * @param points 
+ * @returns 
+ */
+function numberOfBoomerangs(points: number[][]): number {
+  let ans: number = 0
+  for (const p of points) {
+    const cnt = new Map()
+    for (const q of points) {
+      const dis = (p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1])
+      cnt.set(dis, (cnt.get(dis) || 0) + 1)
+    }
+    for (const [_, m] of cnt.entries()) {
+      ans += m * (m - 1)
+    }
+  }
+  return ans
+}
