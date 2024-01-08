e92685265f3f9496e9e1d34e3bef9398ab3c8b28
ll799268 <1010023095@qq.com>
2024-01-08 22:40:57 +0800

    feat: 检查是否是类的对象实例

--- leetCode/checkIfInstanceOf.ts
+++ leetCode/checkIfInstanceOf.ts
@@ -1,10 +1,7 @@
 /**
  * 检查是否是类的对象实例
- * @param {*} obj
- * @param {*} classFunction
- * @return {boolean}
  */
-var checkIfInstanceOf = function (obj, classFunction) {
+function checkIfInstanceOf(obj: any, classFunction: any): boolean {
   if (obj === null || obj === undefined || !(classFunction instanceof Function)) return false;
   return Object(obj) instanceof classFunction;
 };
