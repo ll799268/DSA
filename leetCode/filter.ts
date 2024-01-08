3609eebeafbf5aecbffadc0848fe9b4b4e038186
ll799268 <1010023095@qq.com>
2024-01-08 22:16:08 +0800

    feat: 过滤数组中的元素

--- leetCode/filter.ts
+++ leetCode/filter.ts
@@ -3,9 +3,6 @@ type Fn = (n: number, i: number) => any
 
 /**
  * 过滤数组中的元素
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
  */
 function filter(arr: number[], fn: Fn): number[] {
   let result: number[] = []
