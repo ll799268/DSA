ac33c464e3c23500858a6e97a8f9678b587d402d
ll799268 <1010023095@qq.com>
2024-01-09 21:57:17 +0800

    feat: 嵌套数组生成器

---
+++ leetCode/inorderTraversal.ts
@@ -0,0 +1,21 @@
+type MultidimensionalArray = (MultidimensionalArray | number)[]
+
+/**
+ * 嵌套数组生成器
+ */
+function* inorderTraversal(arr: MultidimensionalArray): Generator<number, void, unknown> {
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      yield* inorderTraversal(item)
+    } else {
+      yield item
+    }
+  }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
