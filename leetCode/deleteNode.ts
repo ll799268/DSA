0edac933f4ff22270325daf0c88e76753af94c12
ll799268 <1010023095@qq.com>
2024-01-11 17:08:13 +0800

    feat: 删除链表的节点

---
+++ leetCode/deleteNode.ts
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+/**
+ * 删除链表的节点
+ */
+function deleteNode(head: ListNode | null, val: number): ListNode | null {
+  let pre = new ListNode()
+  pre.next = head
+  let p = pre
+  while (p.next) {
+    if (p.next.val === val) {
+      p.next = p.next.next
+    } else {
+      p = p.next
+    }
+  }
+
+  return pre.next
+};
