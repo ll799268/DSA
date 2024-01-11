2b803460979b36a7760ab2cfc6c3b3f1a165bcd8
ll799268 <1010023095@qq.com>
2024-01-11 17:47:27 +0800

    feat: 仅仅反转字母

---
+++ leetCode/reverseOnlyLetters.ts
@@ -0,0 +1,22 @@
+/**
+ * 仅仅反转字母
+ */
+function reverseOnlyLetters(s: string): string {
+  const arr: string[] = s.split('')
+  let i: number = 0,
+    j: number = arr.length - 1
+
+  while (i < j) {
+    if (!/[a-zA-Z]/.test(arr[i])) {
+      i++
+      continue
+    }
+    if (!/[a-zA-Z]/.test(arr[j])) {
+      j--
+      continue
+    }
+    [arr[i], arr[j]] = [arr[j], arr[i]]
+  }
+
+  return arr.join('')
+};
