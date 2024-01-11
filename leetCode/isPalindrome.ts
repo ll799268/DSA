5106ccbbebae69b6ff9a4334c893e30c3847579c
ll799268 <1010023095@qq.com>
2024-01-11 17:26:14 +0800

    feat: 回文链表

---
+++ leetCode/isPalindrome.ts
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+/**
+ * 回文链表
+ */
+function isPalindrome(head: ListNode | null): boolean {
+  const arr: number[] = []
+  while (head) {
+    arr.push(head.val)
+    head = head.next
+  }
+
+  for (let i = 0, j = arr.length - 1; i < arr.length, j > 0; i++, j--) {
+    if (arr[i] !== arr[j]) return false
+  }
+
+  return true
+};
+function isPalindrome1(head: ListNode | null): boolean {
+  let frontPointer: ListNode | null | any = null
+  const recursivelyCheck = (currentNode: ListNode | null) => {
+    if (currentNode !== null) {
+      if (!recursivelyCheck(currentNode.next)) return false
+      if (currentNode.val !== frontPointer.val) return false
+      frontPointer = frontPointer.next
+    }
+    return true
+  }
+
+  frontPointer = head
+  return recursivelyCheck(head)
+};
