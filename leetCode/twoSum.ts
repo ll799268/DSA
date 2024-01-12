6e737bc935df597b72085573dd8e0d17cf16d494
ll799268 <1010023095@qq.com>
2024-01-12 15:17:50 +0800

    feat: 两数之和

---
+++ leetCode/twoSum.ts
@@ -0,0 +1,30 @@
+/**
+ * 两数之和
+ */
+function twoSum(nums: number[], target: number): number[] {
+  const map: Map<number, number> = new Map();
+  let res: number[] = []
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      res = [map.get(nums[i]) as number, i]
+      break
+    }
+    map.set(target - nums[i], i)
+  }
+
+  return res
+};
+
+function twoSum1(nums: number[], target: number): number[] {
+  let res: number[] = []
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] + nums[j] === target) {
+        res = [i, j]
+        return res
+      }
+    }
+  }
+
+  return res
+};
