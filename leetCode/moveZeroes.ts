8aff0cc107a4991bae4541b37a0e9a26e12ec4bd
ll799268 <1010023095@qq.com>
2024-01-12 19:22:30 +0800

    feat: 移动零

--- leetCode/moveZeroes.ts
+++ leetCode/moveZeroes.ts
@@ -1,10 +1,9 @@
 /**
  * 移动零
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function (nums) {
-  let count = 0
+function moveZeroes(nums: number[]): void {
+  let count: number = 0
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === 0) {
       nums.splice(i, 1)
@@ -13,19 +12,19 @@ var moveZeroes = function (nums) {
     }
   }
 
+  // while (count--) {
+  //   nums.push(0)
+  // }
+
   nums.push(...new Array(count).fill(0))
-  return nums
 };
 
-var moveZeroes = function (nums) {
+function moveZeroes1(nums: number[]): void {
   let slowIndex = 0
-
-  for (let fastIndex = 0; i < nums.length; fastIndex++) {
+  for (let fastIndex = 0; fastIndex < nums.length; fastIndex++) {
     if (nums[fastIndex] !== 0) {
       [nums[slowIndex], nums[fastIndex]] = [nums[fastIndex], nums[slowIndex]]
       slowIndex++
     }
   }
-}
-
-console.log(moveZeroes([0, 0, 1]));
+};
