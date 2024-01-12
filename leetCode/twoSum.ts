60ef40d919609485de1f7ae10aa58a62df527542
ll799268 <1010023095@qq.com>
2024-01-12 15:42:52 +0800

    feat: 两数之和

--- leetCode/twoSum.ts
+++ leetCode/twoSum.ts
@@ -6,8 +6,7 @@ function twoSum(nums: number[], target: number): number[] {
   let res: number[] = []
   for (let i = 0; i < nums.length; i++) {
     if (map.has(nums[i])) {
-      res = [map.get(nums[i]) as number, i]
-      break
+      return [map.get(nums[i])!, i]
     }
     map.set(target - nums[i], i)
   }
