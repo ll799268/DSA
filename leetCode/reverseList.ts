ccd2bd22e78a9429ceebfece05e369431cfedbd1
ll799268 <1010023095@qq.com>
2024-01-15 18:52:08 +0800

    feat: 翻转链表

--- leetCode/reverseList.ts
+++ leetCode/reverseList.ts
@@ -8,7 +8,7 @@ function reverseList(head: ListNode | null): ListNode | null {
   let cur: ListNode | null = head
 
   while (cur) {
-    const next = cur.next
+    const next: ListNode | null = cur.next
     cur.next = pre
     pre = cur
     cur = next
@@ -17,3 +17,23 @@ function reverseList(head: ListNode | null): ListNode | null {
   return pre
 
 };
+
+/**
+ * 递归(前 => 后)
+ */
+type Recur = (preNode: ListNode | null, curNode: ListNode | null) => ListNode | null
+
+function reverseList1(head: ListNode | null): ListNode | null {
+  const recur: Recur = (preNode, curNode) => {
+    if (curNode === null) return preNode
+
+    const tempNode: ListNode | null = curNode.next
+    curNode.next = preNode
+    preNode = curNode
+    curNode = tempNode
+
+    return recur(preNode, curNode)
+  }
+
+  return recur(null, head)
+}
