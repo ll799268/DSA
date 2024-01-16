2cd004ddbe6d96e361a1bdc4392f82650c4aa02f
ll799268 <1010023095@qq.com>
2024-01-16 22:06:53 +0800

    feat: 删除有字符串中的所以相邻重复项

---
+++ leetCode/removeDuplicatesString.ts
@@ -0,0 +1,18 @@
+/** 
+ * 删除有字符串中的所以相邻重复项
+ */
+function removeDuplicates(s: string): string {
+  const stack: string[] = []
+  let i: number = 0
+  while (i < s.length) {
+    const cur: string = s[i]
+    if (cur === stack[stack.length - 1]) {
+      stack.pop()
+    } else {
+      stack.push(cur)
+    }
+    i++
+  }
+
+  return stack.join('')
+}
