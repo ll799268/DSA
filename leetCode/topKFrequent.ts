4faede46118379944034ad6ab9d56e535dc9fe91
ll799268 <1010023095@qq.com>
2024-01-17 17:50:20 +0800

    feat: 前K个高频元素

---
+++ leetCode/topKFrequent.ts
@@ -0,0 +1,15 @@
+/**
+ * 前K个高频元素
+ */
+function topKFrequent(nums: number[], k: number): number[] {
+  const map: Map<number, number> = new Map()
+
+  for (const n of nums) {
+    map.set(n, map.get(n) ?? 0 + 1)
+  }
+
+  return [...map.entries()]
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, k)
+    .map(i => i[0])
+};
