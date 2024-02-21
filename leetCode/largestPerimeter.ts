a68c0f706cf49d6fac5b52865463baa931686e25
ll799268 <1010023095@qq.com>
2024-02-21 19:05:24 +0800

    feat: 三角形的最大周长

---
+++ leetCode/largestPerimeter.ts
@@ -0,0 +1,12 @@
+/**
+ * 三角形的最大周长
+ */
+function largestPerimeter(nums: number[]): number {
+  nums.sort((a, b) => a - b)
+  for (let i = nums.length - 1; i >= 2; --i) {
+    if (nums[i - 2] + nums[i - 1] > nums[i]) {
+      return nums[i - 2] + nums[i - 1] + nums[i]
+    }
+  }
+  return 0
+};
