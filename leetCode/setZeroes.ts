10ab7876de5fd6704ed7861c95d6178f0a0a95e1
ll799268 <1010023095@qq.com>
2024-02-21 18:56:44 +0800

    feat: 矩阵置零

---
+++ leetCode/setZeroes.ts
@@ -0,0 +1,29 @@
+/**
+ * 矩阵置零
+ * Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+  let m = matrix.length
+  let n = matrix[0].length
+
+  let rows = new Array(m).fill(false)
+  let cols = new Array(n).fill(false)
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === 0) {
+        rows[i] = cols[j] = true
+      }
+    }
+  }
+
+
+  for (let i = 0; i < rows.length; i++) {
+    for (let j = 0; j < cols.length; j++) {
+      if (rows[i] || cols[j]) {
+        matrix[i][j] = 0
+      }
+    }
+  }
+
+};
