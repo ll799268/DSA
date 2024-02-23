8dab813dc6179279250ab790e6b8eec669fe6dec
ll799268 <1010023095@qq.com>
2024-02-23 16:36:12 +0800

    feat: 颜色分类

---
+++ leetCode/sortColors.ts
@@ -0,0 +1,21 @@
+/**
+ * 颜色分类
+ * Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): void {
+  const map: Map<number, number> = new Map()
+  map.set(0, 0)
+  map.set(1, 0)
+  map.set(2, 0)
+
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], map.get(nums[i]) + 1)
+  }
+
+  let i: number = 0
+  for (let [k, v] of map.entries()) {
+    while (v--) {
+      nums[i++] = k
+    }
+  }
+};
