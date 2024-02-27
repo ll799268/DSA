feb176a2a77128ee36eed27c3758242f6f5e7a8f
ll799268 <1010023095@qq.com>
2024-02-27 15:06:58 +0800

    feat: 回文字符串

--- leetCode/isPalindrome.ts
+++ leetCode/isPalindrome.ts
@@ -1,32 +1,23 @@
-import ListNode from './ListNode'
-
-/**
- * 回文链表
+/** 
+ * 回文字符串
  */
-function isPalindrome(head: ListNode | null): boolean {
-  const arr: number[] = []
-  while (head) {
-    arr.push(head.val)
-    head = head.next
-  }
 
-  for (let i = 0, j = arr.length - 1; i < arr.length, j > 0; i++, j--) {
-    if (arr[i] !== arr[j]) return false
+function isPalindrome(s: string): boolean {
+  const code: string = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+
+  for (let i = 0, j = code.length - 1; i < j; i++, j--) {
+    if (code[i] !== code[j]) return false
   }
 
   return true
 };
-function isPalindrome1(head: ListNode | null): boolean {
-  let frontPointer: ListNode | null | any = null
-  const recursivelyCheck = (currentNode: ListNode | null) => {
-    if (currentNode !== null) {
-      if (!recursivelyCheck(currentNode.next)) return false
-      if (currentNode.val !== frontPointer.val) return false
-      frontPointer = frontPointer.next
-    }
-    return true
+
+function isPalindrome1(s: string): boolean {
+  const code: string = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+
+  for (let i = 0; i < Math.floor(code.length / 2); i++) {
+    if (code[i] !== code[code.length - 1 - i]) return false
   }
 
-  frontPointer = head
-  return recursivelyCheck(head)
+  return true
 };
