163b09a0ce13d10d4d79d8aa9411d814e49ff22f
ll799268 <1010023095@qq.com>
2024-02-27 18:12:04 +0800

    feat: 无重复字符的最长子串

--- leetCode/lengthOfLongestSubstring.ts
+++ leetCode/lengthOfLongestSubstring.ts
@@ -5,8 +5,11 @@ function lengthOfLongestSubstring(s: string): number {
   let minIndex: number = 0
   let ans: number = 0
   for (let i = 0; i < s.length; i++) {
-    if (s.indexOf(s[i], minIndex) < i) minIndex = s.indexOf(s[i], minIndex) + 1
-    else ans = Math.max(ans, i - minIndex + 1)
+    if (s.indexOf(s[i], minIndex) < i) {
+      minIndex = s.indexOf(s[i], minIndex) + 1
+      continue
+    }
+    ans = Math.max(ans, i - minIndex + 1)
   }
   return ans
 }
