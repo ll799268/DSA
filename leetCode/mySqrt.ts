e7c7aee3bfda38b477360b6a391703c34c59dd66
ll799268 <1010023095@qq.com>
2024-03-04 18:39:08 +0800

    feat: x 的平方根

---
+++ leetCode/mySqrt.ts
@@ -0,0 +1,14 @@
+/** 
+ * x 的平方根
+ */
+function mySqrt(x: number): number {
+  let i = 1
+  while (true) {
+    if (i * i > x) return i - 1
+    i++
+  }
+}
+
+function mySqrt1(x: number): number {
+  return Math.floor(Math.sqrt(x))
+}
