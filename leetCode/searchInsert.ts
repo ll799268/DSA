762758c0cfeab9ee55aa723bdc114ffe0f02a0a3
ll799268 <1010023095@qq.com>
2024-03-17 22:54:53 +0800

    feat: 搜索插入位置

--- leetCode/searchInsert.ts
+++ leetCode/searchInsert.ts
@@ -1,10 +1,7 @@
-/** 搜索插入位置
- * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
+/** 
+ * 搜索插入位置
  */
-var searchInsert = function (nums, target) {
+function searchInsert(nums: number[], target: number): number {
   if (nums.indexOf(target) > -1) return nums.indexOf(target)
 
   for (let i = 0; i < nums.length; i++) {
@@ -15,8 +12,7 @@ var searchInsert = function (nums, target) {
   return nums.length
 }
 
-
-var searchInsert = function (nums, target) {
+function searchInsert1(nums: number[], target: number): number {
   let [left, right] = [0, nums.length]
 
   while (left <= right) {
