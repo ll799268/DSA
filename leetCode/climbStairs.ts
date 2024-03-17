58298c2f379197bfaad2af88824b1a22e2e9aebc
ll799268 <1010023095@qq.com>
2024-03-17 22:47:48 +0800

    feat: 爬楼梯

---
+++ leetCode/climbStairs.ts
@@ -0,0 +1,14 @@
+/** 
+ * 爬楼梯
+ */
+function climbStairs(n: number): number {
+  // dp[i] 为第 i 阶楼梯有多少种方法爬到楼顶
+  // dp[i] = dp[i - 1] + dp[i - 2]
+  let dp = [1, 2]
+  for (let i = 2; i < n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2]
+  }
+  return dp[n - 1]
+}
+
+console.log(climbStairs(5))
